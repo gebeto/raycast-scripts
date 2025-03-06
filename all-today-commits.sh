9f80c9dfde33380959d9640940fb93b2e4344023
gebeto <slavik.nychkalo@gmail.com>
2025-03-06 14:01:00 +0200

    add get today commits script

---
+++ all-today-commits.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title All Today commits
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Get all today commits
+# @raycast.author gebeto
+# @raycast.authorURL https://raycast.com/gebeto
+
+~/Desktop/Work/uplift/uplift-workspace/today.sh
